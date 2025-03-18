fd759b48d2135132a4b9d7dc36a04d5458c466c1
Jakub Skowron <jakubskowron676@gmail.com>
2025-03-18 17:31:40 +0100

    feat: add new `--rerun` option
    --rerun allows for execution of a single step in case of an error
    happening while executing all of dotsetup.
    
    it will only rerun the specified step, after which it will exit.
    user can also specify the --dry-run flag after --rerun to dry run
    specified step first.
    
    available steps (IDs): packages, system, home, suckless

--- utils.sh
+++ utils.sh
@@ -12,7 +12,86 @@ fi
 
 __dotsetup_sudo_user="${USER} ALL=(ALL) NOPASSWD: ALL"
 
-__dotsetup_version="v1.2.3"
+__dotsetup_version="v1.3.0"
+
+__dotsetup_cli()
+{
+    if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
+        __dotsetup_help
+        exit 0
+    elif [ "$1" = "--version" ] || [ "$1" = "-v" ]; then
+        __dotsetup_version
+        exit 0
+    elif [ "$1" = "--dry-run" ] || [ "$1" = "-d" ]; then
+        __dotsetup_log WARNING "Dry running...\n"
+        __dotsetup_dry_ran=true
+        __dotsetup_main
+    elif [ "$1" = "--rerun" ] || [ "$1" = "-r" ]; then
+        if echo "$@" | grep -q "\--dry-run" || echo "$@" | grep -q "\-d"; then
+            __dotsetup_log WARNING "Dry running...\n"
+            __dotsetup_dry_ran=true
+        else
+            __dotsetup_dry_ran=false
+        fi
+
+        shift
+
+        if [ "$1" = "packages" ]; then
+            __dotsetup_log INFO "Rerunning package installation..."
+            __dotsetup_rerun "install-packages.sh"
+        elif [ "$1" = "system" ]; then
+            __dotsetup_log INFO "Rerunning system-wide configuration..."
+            __dotsetup_rerun "setup-system-configs.sh"
+        elif [ "$1" = "home" ]; then
+            __dotsetup_log INFO "Rerunning home configuration..."
+            __dotsetup_rerun "setup-home.sh"
+        elif [ "$1" = "suckless" ]; then
+            __dotsetup_log INFO "Rerunning suckless configuration..."
+            __dotsetup_rerun "setup-suckless.sh"
+        else
+            __dotsetup_log ERROR "Invalid step ID! Exiting..."
+            __dotsetup_log INFO "Available IDs: packages, system, home, suckless."
+        fi
+
+        exit 0
+    else
+        __dotsetup_dry_ran=false
+        __dotsetup_main
+    fi
+}
+
+__dotsetup_main()
+{
+    __dotsetup_setup_sudoers_file
+
+    __dotsetup_start_measuring_time
+
+    source "$(dirname "$0")/install-packages.sh"
+    source "$(dirname "$0")/setup-system-configs.sh"
+    source "$(dirname "$0")/setup-home.sh"
+    source "$(dirname "$0")/setup-suckless.sh"
+
+    __dotsetup_end_measuring_time
+
+    __dotsetup_calculate_time
+
+    __dotsetup_delete_backups
+}
+
+__dotsetup_rerun()
+{
+    __dotsetup_setup_sudoers_file
+
+    __dotsetup_start_measuring_time
+
+    source "$(dirname "$0")/$1"
+
+    __dotsetup_end_measuring_time
+
+    __dotsetup_calculate_time
+
+    __dotsetup_delete_backups
+}
 
 __dotsetup_log()
 {
@@ -66,6 +145,7 @@ __dotsetup_delete_backups()
                 for __dotsetup_backup in "${__dotsetup_backups[@]}"; do
                     if [ -e "${__dotsetup_backup}" ]; then
                         __dotsetup_execute 'sudo rm -rf "${__dotsetup_backup}"'
+                        __dotsetup_check_last_command
                         __dotsetup_log SUCCESS "Successfully deleted ${__dotsetup_backup}!"
                     else
                         __dotsetup_log INFO "${__dotsetup_backup} does not exist."
@@ -90,16 +170,6 @@ __dotsetup_check_last_command()
     fi
 }
 
-__dotsetup_dry_run()
-{
-    if [ "$1" = "--dry-run" ] || [ "$1" = "-d" ]; then
-        __dotsetup_log WARNING "Dry running...\n"
-        __dotsetup_dry_ran=true
-    else
-        __dotsetup_dry_ran=false
-    fi
-}
-
 __dotsetup_execute()
 {
     if ${__dotsetup_dry_ran}; then
@@ -109,7 +179,9 @@ __dotsetup_execute()
     fi
 }
 
-__dotsetup_start_measuring_time() {
+__dotsetup_start_measuring_time()
+{
+    __dotsetup_execute 'sudo hwclock --systohc'
     __dotsetup_start_time=$(date +%s)
 }
 
@@ -126,6 +198,13 @@ __dotsetup_calculate_time()
 
 __dotsetup_setup_sudoers_file()
 {
+    __dotsetup_log WARNING "To execute commands which require elevated permissions and keep dotsetup"
+    __dotsetup_log WARNING "running without sudo interrupting, it requires your password at the start"
+    __dotsetup_log WARNING "to keep the sudo session running, dotsetup will then add your user to"
+    __dotsetup_log WARNING "the /etc/sudeors file so sudo will not interrupt in the middle of"
+    __dotsetup_log WARNING "execution. Don't worry, /etc/sudoers will be reset to its previous"
+    __dotsetup_log WARNING "state after dotsetup is finished."
+
     __dotsetup_execute 'echo "${__dotsetup_sudo_user}" | sudo -E -- tee -a /etc/sudoers >/dev/null'
 
     trap __dotsetup_reset_sudoers_file EXIT
@@ -142,12 +221,13 @@ __dotsetup_reset_sudoers_file()
 
 __dotsetup_help()
 {
-    echo -e "Usage: ./dotsetup [OPTIONS]"
+    echo -e "Usage: dotsetup [-h || -v || -d || -r <packages || system || home || suckless> [-d]]"
     echo
-    echo -e "OPTIONS:"
+    echo -e "Options:"
     echo -e "\t--help,    -h    Print this message and exit."
     echo -e "\t--version, -v    Print dotsetup version and exit."
     echo -e "\t--dry-run, -d    Print setup commands, do not execute them."
+    echo -e "\t--rerun,   -r    Rerun specified step and quit."
 }
 
 __dotsetup_version()
