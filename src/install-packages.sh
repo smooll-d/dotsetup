04c16af6f6558001ab5294d251ca338b8454e457
Jakub Skowron <jakubskowron676@gmail.com>
2025-05-21 21:41:09 +0200

    feat: I don't know

--- src/install-packages.sh
+++ src/install-packages.sh
@@ -18,7 +18,7 @@ fi
 __dotsetup_log INFO "Installing packages from packages-pacman.txt..."
 
 # Install all packages from core, extra, multilib and testing repos
-__dotsetup_execute 'sudo pacman -Sy --noconfirm --needed vulkan-radeon lib32-vulkan-radeon virtualbox-host-modules-arch man-db - < "$(dirname "$0")/packages/packages-pacman.txt"'
+__dotsetup_execute 'sudo pacman -Sy --noconfirm --needed - < "$(dirname "$0")/packages/packages-pacman.txt"'
 
 __dotsetup_log INFO "Installing packages from packages-aur.txt..."
 
